74a910c3461965181446aa3b67c1a929548bc283
Finn Buessen <fbuessen@physics.utoronto.ca>
2021-04-16 11:33:07 -0400

    Fixed mixup of x- and y-axis in structure factor plot

--- examples/honeycomb-structurefactor.jl
+++ examples/honeycomb-structurefactor.jl
@@ -22,25 +22,26 @@ beta = 10.0
 m = MonteCarlo(lattice, beta, thermalizationSweeps, measurementSweeps)
 run!(m)
 
-# Fourier transform correlations to compute structure factor
+# Fourier transform correlations to compute structure factor. 
 N = 256
-correlation = mean(m.observables.correlation)
+correlation = mean(m.observables.correlation) # The correlation is measured with respect to the first spin, i.e. the i-th entry is the correlation dot(S_1,S_i). 
 kx = collect(range(-2pi,2pi,length=N))
 ky = collect(range(-2pi,2pi,length=N))
 structurefactor = zeros(N,N)
 for i in 1:N
     for j in 1:N
         z = 0.0
+        # Compute Fourier transformation at momentum (kx, ky). The real-space position of the i-th spin is obtained via getSitePosition(lattice,i). 
         for k in 1:length(lattice)
             z += cos(dot((kx[i],ky[j]),getSitePosition(lattice,k))) * correlation[k]
         end
-        structurefactor[i,j] = z / length(lattice)
+        structurefactor[j,i] = z / length(lattice)
     end
 end
 
 # Plot result
 using Plots
 heatmap(kx,ky,structurefactor,aspect_ratio=1,xrange=(-2pi,2pi),yrange=(-2pi,2pi),clims=(0,1),xlabel="kx", ylabel="ky")
-xs = [0.0, 2.0pi/sqrt(3.0), 2.0pi/sqrt(3.0), 0.0, -2.0pi/sqrt(3.0), -2.0pi/sqrt(3.0), 0.0]
-ys = [4.0pi/3.0, 2.0pi/3.0, -2.0pi/3.0, -4.0pi/3.0, -2.0pi/3.0, 2.0pi/3.0, 4.0pi/3.0]
+xs = [4.0pi/3.0, 2.0pi/3.0, -2.0pi/3.0, -4.0pi/3.0, -2.0pi/3.0, 2.0pi/3.0, 4.0pi/3.0]
+ys = [0.0, 2.0pi/sqrt(3.0), 2.0pi/sqrt(3.0), 0.0, -2.0pi/sqrt(3.0), -2.0pi/sqrt(3.0), 0.0]
 plot!(xs, ys, label="Extended BZ")
